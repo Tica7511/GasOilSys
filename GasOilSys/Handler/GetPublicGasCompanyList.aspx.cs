e7b219f91ec4e0c9690fc5aa45381555419f0b5c
Tica7511 <ticagaya1234@gmail.com>
2025-02-14 15:46:27 +0800

    [新增] 新增公用天然氣頁面(尚未完成)

---
+++ GasOilSys/Handler/GetPublicGasCompanyList.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Xml;
+public partial class Handler_GetPublicGasCompanyList : System.Web.UI.Page
+{
+    PublicGas_DB db = new PublicGas_DB();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        ///-----------------------------------------------------
+        ///功    能: 查詢公用天然氣業者清單
+        ///說    明:
+        /// * Request["year"]: 年度
+        ///-----------------------------------------------------
+        XmlDocument xDoc = new XmlDocument();
+        try
+        {
+            string year = (string.IsNullOrEmpty(Request["year"])) ? "" : Request["year"].ToString().Trim();
+
+            //string mGuid = string.Empty;
+            //if (LogInfo.competence == "01")
+            //    mGuid = LogInfo.mGuid;
+            //else
+            //    year = string.Empty;
+
+            DataTable dt = db.GetCompanyList();
+
+            string xmlstr = string.Empty;
+            xmlstr = DataTableToXml.ConvertDatatableToXML(dt, "dataList", "data_item");
+            xmlstr = "<?xml version='1.0' encoding='utf-8'?><root>" + xmlstr + "</root>";
+            xDoc.LoadXml(xmlstr);
+        }
+        catch (Exception ex)
+        {
+            xDoc = ExceptionUtil.GetExceptionDocument(ex);
+        }
+        Response.ContentType = System.Net.Mime.MediaTypeNames.Text.Xml;
+        xDoc.Save(Response.Output);
+    }
+}
---
+++ GasOilSys/WebPage/PublicGasInfo.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class WebPage_PublicGasInfo : System.Web.UI.Page
+{
+    public string username;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (LogInfo.mGuid == "")
+            Response.Write("<script>alert('請重新登入'); location='SignIn.aspx';</script>");
+        else
+        {
+            DataTable isdt = Account.IfAccountPasswordEqual(LogInfo.mGuid);
+            if (isdt.Rows.Count > 0)
+            {
+                if (isdt.Rows[0]["ifEqual"].ToString().Trim() == "Y")
+                {
+                    Response.Redirect("~/WebPage/ChangePassword.aspx");
+                }
+            }
+
+            if (LogInfo.competence == "02")
+                Response.Redirect("~/WebPage/GasInfo.aspx?cp=" + LogInfo.companyGuid);
+
+            username = LogInfo.name;
+        }
+    }
+}
